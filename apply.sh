b2279a0eda8bdacab024e1e9ac5482c84bd63692
daquexian <daquexian566@gmail.com>
2019-01-16 22:02:17 +0800

    Add coc-settings.json and apply.sh

---
+++ apply.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/env bash
+
+ln -sf $PWD/.vimrc ~/
+cp .vimrc.local ~/
+ln -sf $PWD/.tmux.conf ~/
+ln -sf $PWD/.tmux.conf.local ~/
+ln -sf $PWD/.zshrc ~/
+ln -sf $PWD/.gitconfig ~/
+ln -sf $PWD/coc-settings.json ~/.config/nvim/
